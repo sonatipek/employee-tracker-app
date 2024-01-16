027087126775f7233fd90e2918d45929a4187d77
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-01-16 12:26:01 +0300

    title component optimization for mobile devices

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ additionalClasses, children }) {
   return (
-    <h1 className={`text-pretty text-5xl font-bold ${additionalClasses || ""}`}>
+    <h1 className={`text-pretty text-3xl md:text-5xl font-bold ${additionalClasses || ""}`}>
       {children}
     </h1>
   );
