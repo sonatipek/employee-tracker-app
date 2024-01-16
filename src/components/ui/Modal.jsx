2e7d6118796d67810ba51133c5e3cb1b8e0f18fb
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-01-16 13:06:30 +0300

    modal component optimized for mobile devices

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -7,11 +7,11 @@ import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 export default function Modal({ error, buttonEvent }) {
   return (
     <div className="fixed top-0 pt-[15svh] left-0 flex justify-center mx-auto w-screen h-screen z-50 backdrop-blur-sm backdrop-brightness-50">
-      <div className="w-2/5">
+      <div className="w-full px-4 md:w-2/5">
         <Card additionalClasses="h-fit">
           <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-            <div className="flex gap-2 justify-start border-b pb-3">
-              <ExclamationTriangleIcon className="size-6 text-red-600" />
+            <div className="flex gap-2 justify-start items-end border-b pb-3">
+              <ExclamationTriangleIcon className="size-6 md:size-12 text-red-600" />
               <Title additionalClasses={"text-xl leading-6 font-semibold"}>
                 {error.title}
               </Title>
